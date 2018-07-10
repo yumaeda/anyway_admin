32a46db15c4054c1125b4718e249794eaafa43fd
Yukitaka Maeda <yumaeda@gmail.com>
2018-07-10 23:52:37 +0900

    Goods Issue: Move JS files

--- goods_issue/install.sh
+++ goods_issue/install.sh
@@ -1,8 +1,8 @@
-# Generates JS
-cat "../vendor/extensions.js"      > "./build/index.js"
-cat "../vendor/HtmlControl.js"    >> "./build/index.js"
-cat "../vendor/jquery-barcode.js" >> "./build/index.js"
-cat "./imports.js"                >> "./build/index.js"
-cat "./GoodsIssueControl.js"      >> "./build/index.js"
-cat "./index.js"                  >> "./build/index.js"
+# Generate JS
+cat "../vendor/extensions.js"        > "./build/index.js"
+cat "../vendor/HtmlControl.js"      >> "./build/index.js"
+cat "../vendor/jquery-barcode.js"   >> "./build/index.js"
+cat "./src/js/imports.js"           >> "./build/index.js"
+cat "./src/js/GoodsIssueControl.js" >> "./build/index.js"
+cat "./src/jsindex.js"              >> "./build/index.js"
 
