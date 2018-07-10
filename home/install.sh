f1680aab1980de6d024e333fd1f69d2b40101c10
Yukitaka Maeda <yumaeda@gmail.com>
2018-07-10 23:55:28 +0900

    Home: Move JS & CSS files

--- home/install.sh
+++ home/install.sh
@@ -1,9 +1,9 @@
 # Generate JS
-cat "../vendor/extensions.js" > "./build/index.js"
-cat "./imports.js"           >> "./build/index.js"
-cat "./AdminHomePage.js"     >> "./build/index.js"
-cat "./index.js"             >> "./build/index.js"
+cat "../vendor/extensions.js"    > "./build/index.js"
+cat "./src/js/imports.js"       >> "./build/index.js"
+cat "./src/js/AdminHomePage.js" >> "./build/index.js"
+cat "./src/js/index.js"         >> "./build/index.js"
 
 # Generate CSS
-cat "./css/index.css" > "./build/index.css"
+cat "./src/css/index.css" > "./build/index.css"
 
