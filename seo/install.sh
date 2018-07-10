fed6d0dcba7c4e4ffcf4363f5d7e74b5ff6143b2
Yukitaka Maeda <yumaeda@gmail.com>
2018-07-11 00:03:35 +0900

    SEO: Move JS files

--- seo/install.sh
+++ seo/install.sh
@@ -1,6 +1,6 @@
 # Generate JS
 cat "../vendor/extensions.js" > "./build/index.js"
 cat "../vendor/jquery.js"    >> "./build/index.js"
-cat "./imports.js"           >> "./build/index.js"
-cat "./producers.js"         >> "./build/index.js"
+cat "./src/js/imports.js"    >> "./build/index.js"
+cat "./src/js/producers.js"  >> "./build/index.js"
 
