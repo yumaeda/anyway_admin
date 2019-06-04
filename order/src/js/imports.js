194bd1698f7663d7bbc9dd9de88bbb3fb79285d3
Yukitaka Maeda <yumaeda@gmail.com>
2019-06-04 23:28:25 +0900

    Remove old code

--- order/src/js/imports.js
+++
@@ -1,3 +0,0 @@
-import { TableTag, TableRow, TableColumn, ImageTag, SelectTag, InputTag, ButtonTag } from '../../../modules/build/html_tags';
-import { HiddenField } from '../../../modules/build/fields';
-import UrlQuery from '../../../modules/build/UrlQuery';
--- order/src/js/index.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function()
-{
-    var orderMgr = new OrderManager();
-    orderMgr.render();
-});
-
