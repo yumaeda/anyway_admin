6f01fa4f4e9c3c6cf9f76453ba71db12d9c9d7e7
Yukitaka Maeda <yumaeda@gmail.com>
2021-09-29 11:26:11 +0900

    Add DB management files

---
+++ new_info/dbTables.php
@@ -0,0 +1,29 @@
+<?php
+
+$dbTables = array(
+    "importers" => array(
+        "sort_order",
+        "name",
+        "address",
+        "tel",
+        "email",
+        "dir",
+        "filename",
+        "url"
+    ),
+
+    "new_info" => array(
+        "sort_order",
+        "year",
+        "month",
+        "date",
+        "description"
+    ),
+
+	"price_tags" => array(
+        "barcode_number",
+        "is_printed"
+    )
+);
+
+?>
